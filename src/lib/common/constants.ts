6a89b592c83edd094cb23ea9be4a1c6bdb9b8db0
Zishan <amanzishan.az@gmail.com>
2024-02-15 17:21:28 +0530

    fix(pages): testnet config

--- src/lib/common/constants.ts
+++ src/lib/common/constants.ts
@@ -1,11 +1,11 @@
 import {
   StacksMainnet,
-  StacksMocknet,
-  //StacksTestnet,
+  // StacksMocknet,
+  StacksTestnet,
 } from 'micro-stacks/network';
 
 export const stacksNetwork =
-  process.env.NODE_ENV === 'production' ? StacksMainnet : StacksMocknet;
+  process.env.NODE_ENV === 'production' ? StacksMainnet : StacksTestnet;
 
 export const baseUrl =
   process.env.NODE_ENV === 'production'
