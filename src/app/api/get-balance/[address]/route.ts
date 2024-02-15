03fd19043d7a4b21d73e5282997cdc4773a276f2
Zishan <amanzishan.az@gmail.com>
2024-02-15 18:10:45 +0530

    fix(pages): balance api

--- src/app/api/get-balance/[address]/route.ts
+++ src/app/api/get-balance/[address]/route.ts
@@ -1,9 +1,9 @@
 // src/app/api/get-balance/route.ts
-import { MEGA_GOVERNANCE_CONTRACT } from '@common/constants';
+import { MEGA_GOVERNANCE_CONTRACT, stacksNetwork } from '@common/constants';
 import { tokenToNumber } from '@lib/common/helpers';
-import { fetchReadOnlyFunction } from 'micro-stacks/api';
+// import { fetchReadOnlyFunction } from 'micro-stacks/api';
 import { standardPrincipalCV } from 'micro-stacks/clarity';
-import { StacksTestnet } from 'micro-stacks/network';
+// import { StacksTestnet } from 'micro-stacks/network';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
@@ -11,17 +11,40 @@ export async function GET(
   { params }: { params: { address: string } }
 ) {
   try {
-    const balance: any = await fetchReadOnlyFunction({
-      network: new StacksTestnet(),
-      contractAddress: MEGA_GOVERNANCE_CONTRACT.split('.')[0],
-      contractName: MEGA_GOVERNANCE_CONTRACT.split('.')[1],
-      senderAddress: params.address,
-      functionArgs: [standardPrincipalCV(params.address)],
-      functionName: 'get-balance',
-    });
-    console.log(balance);
+    const network = new stacksNetwork();
+    const url = network.getCoreApiUrl();
 
-    return NextResponse.json({ balance: tokenToNumber(parseInt(balance), 2) });
+    const balance = await fetch(
+      `${url}/v2/contracts/call-read/${
+        MEGA_GOVERNANCE_CONTRACT.split('.')[0]
+      }/${MEGA_GOVERNANCE_CONTRACT.split('.')[1]}/get-balance`,
+      {
+        method: 'POST',
+        cache: 'no-store',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+
+        body: JSON.stringify({
+          sender: params.address,
+          arguments: [standardPrincipalCV(params.address)],
+        }),
+      }
+    );
+    // const balance: any = await fetchReadOnlyFunction({
+    //   network: new StacksTestnet(),
+    //   contractAddress: MEGA_GOVERNANCE_CONTRACT.split('.')[0],
+    //   contractName: MEGA_GOVERNANCE_CONTRACT.split('.')[1],
+    //   senderAddress: params.address,
+    //   functionArgs: [standardPrincipalCV(params.address)],
+    //   functionName: 'get-balance',
+    // });
+    console.log(await balance.json());
+
+    return NextResponse.json({
+      balance: tokenToNumber(parseInt(await balance.json()), 2),
+    });
   } catch (e: any) {
     console.error({ e });
   }
